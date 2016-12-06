3a7e2ebf70ee34a1e7ed3bb3d0406c33bc7f2a0e
cpsavva <csoriasavva@gmail.com>
2016-12-05 22:06:59 -0500

    fixed crystal sum problem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,83 +12,104 @@ var loseCounter = 0;
 //
 //FUNCTIONS
 //function that generates random number between 19 and 120
-function startGame(){
-		numberToMatch = Math.floor(Math.random() * (120 - 19) + 19);
-		$("#numberToMatch").html(numberToMatch);
+	function startGame(){
+			numberToMatch = Math.floor(Math.random() * (120 - 19) + 19);
+			$("#numberToMatch").html(numberToMatch);
 
-		//function that generates random number between 1 and 12
-		redGenerator = Math.floor(Math.random() * (1 - 12) + 12);
-		yellowGenerator = Math.floor(Math.random() * (1 - 12) + 12);
-		blueGenerator = Math.floor(Math.random() * (1 - 12) + 12);
-		greenGenerator = Math.floor(Math.random() * (1 - 12) + 12);
+			//function that generates random number between 1 and 12
+			redGenerator = Math.floor(Math.random() * (1 - 12) + 12);
+			yellowGenerator = Math.floor(Math.random() * (1 - 12) + 12);
+			blueGenerator = Math.floor(Math.random() * (1 - 12) + 12);
+			greenGenerator = Math.floor(Math.random() * (1 - 12) + 12);
 
-		//apply 1/12 generator to crystal buttons
-		//everytime crystal button is clicked, add number to crystalSum
-		$("#redButton").on("click", function(){
-			console.log("red generator " + redGenerator)
-			$("#crystalSum").append(redGenerator);
-		});
-		$("#yellowButton").on("click", function(){
-			console.log("yellow generator " + yellowGenerator)
-			$("#crystalSum").append(yellowGenerator);
+			//apply 1/12 generator to crystal buttons
+			//everytime crystal button is clicked, add number to crystalSum
+			// $("#redButton").on("click", function(){
+			// 	console.log("red generator " + redGenerator)
+			// 	$("#crystalSum").append(redGenerator);
+			// });
+			// $("#yellowButton").on("click", function(){
+			// 	console.log("yellow generator " + yellowGenerator)
+			// 	$("#crystalSum").append(yellowGenerator);
 
-		});
-		$("#blueButton").on("click", function(){
-			console.log("blue generator " + blueGenerator)
-			$("#crystalSum").append(blueGenerator);
-		});
-		$("#greenButton").on("click", function(){
-			console.log("Green Generator " + greenGenerator)
-			$("#crystalSum").append(greenGenerator);
-		});	
-
-
-		//need to find a way to add the value of appendixes together
+			// });
+			// $("#blueButton").on("click", function(){
+			// 	console.log("blue generator " + blueGenerator)
+			// 	$("#crystalSum").append(blueGenerator);
+			// });
+			// $("#greenButton").on("click", function(){
+			// 	console.log("Green Generator " + greenGenerator)
+			// 	$("#crystalSum").append(greenGenerator);
+			// });	
 
 
+			//need to find a way to add the value of appendixes together
+			crystalSum = 0;
+			$("#redButton").on("click", function(){
+	   			crystalSum = Number(crystalSum) + Number(redGenerator);
+	   			 $("#crystalSum").html(crystalSum);        
+			});
+			$("#greenButton").on("click", function(){
+	   			crystalSum = Number(crystalSum) + Number(greenGenerator);
+	   			 $("#crystalSum").html(crystalSum);        
+			});
+			$("#yellowButton").on("click", function(){
+	   			crystalSum = Number(crystalSum) + Number(yellowGenerator);
+	   			 $("#crystalSum").html(crystalSum);        
+			});
+			$("#blueButton").on("click", function(){
+	   			crystalSum = Number(crystalSum) + Number(blueGenerator);
+	   			 $("#crystalSum").html(crystalSum);        
+			});
+
+	  
 
 
 
-		//testing
-		console.log("number to Match: " + numberToMatch);
-		console.log("button generator " + redGenerator);
-		console.log("button generator " + yellowGenerator);
-		console.log("button generator " + blueGenerator);
-		console.log("button generator " + greenGenerator);
 
+			//testing
+			console.log("number to Match: " + numberToMatch);
+			console.log("button generator " + redGenerator);
+			console.log("button generator " + yellowGenerator);
+			console.log("button generator " + blueGenerator);
+			console.log("button generator " + greenGenerator);
 
-	
-
-//conditional if 19/120 number equals crystalSum, add to winCounter;
-	if(numberToMatch === crystalSum){
-		winCounter++;
-
-		$("#winCounter").html(winCounter);
-
-		startGame();
 	}
-	//if 19/120 number equals crystalSum, add to loseCounter; 
-	else if(numberToMatch < crystalSum){
-		loseCounter++;
 
-		$("#loseCounter").html(loseCounter);
+		
 
-		startGame();
+	//conditional if 19/120 number equals crystalSum, add to winCounter;
+	function completeGame () {
+		if(numberToMatch === crystalSum){
+			winCounter++;
+
+			$("#winCounter").html(winCounter);
+
+			startGame();
+		}
+		//if 19/120 number equals crystalSum, add to loseCounter; 
+		else if(Number(numberToMatch) < Number(crystalSum)){
+			loseCounter++;
+
+			$("#loseCounter").html(loseCounter);
+
+			startGame();
+		}
 	}
 
 
-//reset generators
-//reset crystalSum
-
-	};
+	//reset generators
+	//reset crystalSum
 
 
-//
+
+	//
 
 
-//MAIN PROCESS
+	//MAIN PROCESS
 
-startGame();
+	startGame();
+	completeGame ();
 
 
 
